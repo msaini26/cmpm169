63740ed6989f0969b8c25915003fb4a63185305f
Mansi S <mansisaini26@gmail.com>
2024-01-19 08:39:51 -0800

    init experiment 2

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Mansi Saini
+// Date: 1/11/2023
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
--- experiment2/js/project.js
+++ experiment2/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Mansi Saini
+// Date: 1/19/2024
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
