37a09bb534b8bab2b7dc3b0fb156ff55ce34f629
Mansi S <mansisaini26@gmail.com>
2024-02-04 18:23:26 -0800

    init experiment 4

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -14,54 +14,51 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+// imitate: https://openprocessing.org/sketch/909153
+var capture;
+var radius = 20;
+var imgCache;
+let overAllTexture;
 
-    myMethod() {
-        // code to run when method is called
-    }
+class MyClass {
+  constructor(param1, param2) {
+    this.property1 = param1;
+    this.property2 = param2;
+  }
+
+  myMethod() {
+    // code to run when method is called
+  }
 }
 
 // setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
+  // resize canvas is the page is resized
+  $(window).resize(function () {
+    console.log("Resizing...");
+    resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  });
+  // create an instance of the class
+  myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  var centerHorz = windowWidth / 2;
+  var centerVert = windowHeight / 2;
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(220);
+  // call a method on the instance
+  myInstance.myMethod();
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  // Put drawings here
 }
-
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  // code to run when mouse is pressed
+  console.log("hi");
 }
