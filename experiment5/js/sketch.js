afdb40bbebce69eec415856ddeea844056a197e6
Mansi S <mansisaini26@gmail.com>
2024-02-08 15:44:44 -0800

    lighting + rotation

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -14,6 +14,9 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 
+// art results
+let num_points = 100;
+
 class MyClass {
     constructor(param1, param2) {
         this.property1 = param1;
@@ -29,7 +32,7 @@ class MyClass {
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
@@ -41,24 +44,37 @@ function setup() {
 
     var centerHorz = windowWidth / 2;
     var centerVert = windowHeight / 2;
+
+    // imitate: https://openprocessing.org/sketch/1162440
+    // art setup
+    angleMode(DEGREES);
+    colorMode(HSB);
+    noStroke();
 }
 
 // draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
+function draw() {   
     // call a method on the instance
     myInstance.myMethod();
 
     // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    // imitate: https://openprocessing.org/sketch/1162440
+    orbitControl(); // allows the use of the mouse to orbit in 3D
+    scale(0.9); // scale the drawing
+    background(0); // set the background to black
+    translate(0, 0, -150); // use z-axis to move the drawing back
+    
+    // rotate the drawing along the y and z-axis
+    rotateY(frameCount/2.0);
+    rotateZ(frameCount/6.0); 
+
+    // add lighting
+    ambientLight(30);
+
+    // define lighting point locations
+    pointLight(30, 20, 100, 0, 0, 0);
+    pointLight(220, 50, 100, 0, 300, 200);
+    
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
