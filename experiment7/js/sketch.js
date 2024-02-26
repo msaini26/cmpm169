db339955d513925a39d016a1179ac071e2c654d5
Mansi S <mansisaini26@gmail.com>
2024-02-25 20:35:10 -0800

    import fixed

--- experiment7/js/sketch.js
+++ experiment7/js/sketch.js
@@ -18,12 +18,6 @@ let input_data; // store input data
 
 let data_points = []; // store data points
 
-// data fields
-let longtitude;
-let latitude;
-let elevation;
-let change_in_snow_depth;
-
 class MyClass {
   constructor(param1, param2) {
     this.property1 = param1;
@@ -39,12 +33,18 @@ class MyClass {
 function preload() {
   // load text file
   input_data = loadTable(
-    "bcsnowdepthswetrendsbystation1950-2014.csv",
+    "./data/bcsnowdepthswetrendsbystation1950-2014.csv",
     "csv",
     "header"
   );
   console.log(input_data);
 }
+
+// data fields
+let longtitude;
+let latitude;
+let elevation;
+let change_in_snow_depth;
 
 // setup() function is called once when the program starts
 function setup() {
@@ -70,17 +70,36 @@ function setup() {
   change_in_snow_depth = input_data.getColumn("slope_percentperyear");
 
   // iterate through the data rows
-  for (let row = 0; row < input_data.getRowCount(); row++) {
-    let data_row = input_data.getRow(row);
-    let x = map(data_row.get('latitude'), min(latitude), max(latitude), 10, width-10);
-    let y = map(data_row.get('longtitude'), min(longtitude), max(longtitude), 10, height-10);
-    let diameter = map(row.get('slope_percentperyear'), min(change_in_snow_depth), max(change_in_snow_depth), 3, 11);
-    let color = map(data_row.get('elevation'), min(elevation), max(elevation), 30, 255);
+  for (let r = 0; r < input_data.getRowCount(); r++) {
+    let data_row = input_data.getRow(r);
+    let row = input_data.getRow(r);
+    let x = map(
+      row.get("latitude"),
+      min(latitude),
+      max(latitude),
+      10,
+      width - 10
+    );
+    let y = map(
+      row.get("longitude"),
+      min(longtitude),
+      max(longtitude),
+      10,
+      height - 10
+    );
+    let diam = map(
+      row.get("slope_percentperyear"),
+      min(change_in_snow_depth),
+      max(change_in_snow_depth),
+      3,
+      11
+    );
+    let c = map(row.get("elevation"), min(elevation), max(elevation), 30, 255); //180,230
 
     noFill();
 
     // draw the data points
-    data_points.push(new DataPoint(x, y, diameter, color));
+    data_points.push(new Spot(x, y, diam, c));
   }
 }
 
@@ -91,18 +110,72 @@ function draw() {
   myInstance.myMethod();
 
   // Put drawings here
-  var centerHorz = canvasContainer.width() / 2 - 125;
-  var centerVert = canvasContainer.height() / 2 - 125;
-  fill(234, 31, 81);
-  noStroke();
-  rect(centerHorz, centerVert, 250, 250);
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz + 10, centerVert + 200);
+  for (let i = 0; i < data_points.length; i++) {
+    data_points[i].render();
+  }
+
+  push();
+  stroke(255);
+  noFill();
+  strokeWeight(1);
+  textSize(32);
+  text("BC Snow Depth Trends", 600, 450);
+  //textSize(18);
+  //text('Mapped by : location',330,60);
+  //text('elevation',330,90);
+  //text('& slope',330,120);
+  pop();
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
   // code to run when mouse is pressed
 }
+
+class Spot {
+  constructor(x, y, diam, c) {
+    this.x = x;
+    this.y = y;
+    this.diam = diam;
+    this.c = c;
+    this.t = random(2 * PI);
+  }
+
+  render() {
+    stroke(this.c);
+    strokeWeight(random(2, 5));
+    //strokeWeight(2);
+    push();
+    translate(this.x, this.y);
+    beginShape();
+    for (let theta = 0.01; theta <= 2 * PI; theta += 0.01) {
+      // theta increment for resolution
+
+      //adding vertices (the superformula)
+      let rad = this.delta(
+        theta,
+        1, //a
+        1, //b
+        this.diam * 10, //m
+        0.6 * 10, //n1
+        sin(this.t) * 0.2 - 0.01, //n2  //use cos and sin to oscillate shape
+        cos(this.t) * 0.2 - 0.01 // n3
+      ); // these parameters make a circle
+      let x1 = rad * cos(theta) * 50;
+      let y1 = rad * sin(theta) * 50;
+      vertex(x1, y1);
+    }
+
+    endShape();
+    pop();
+    this.t += 0.1;
+  } // end of render
+
+  delta(theta, a, b, m, n1, n2, n3) {
+    return pow(
+      pow(abs(cos((m * theta) / 4.0) / a), n2) +
+        pow(abs(sin((m * theta) / 4.0) / b), n3),
+      -1.0 / n1
+    );
+  }
+} // end of class
